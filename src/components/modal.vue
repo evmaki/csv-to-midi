c650be007900441f8ee32fa5bb6c20e672951a39
evmaki <e.king@utexas.edu>
2022-06-20 16:53:38 -0500

    Tweak styles (more breathing room)

--- src/components/modal.vue
+++ src/components/modal.vue
@@ -99,7 +99,7 @@ export default {
 .modal-container {
   width: 600px;
   margin: 0px auto;
-  padding: 15px;
+  padding: 16px;
   background-color: #fff;
   border-radius: 5px;
   box-shadow: 0 2px 8px rgba(0, 0, 0, .33);
